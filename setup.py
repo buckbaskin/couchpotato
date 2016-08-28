c457ce08d9143b2938635838fc090d668833cb7d
Buck Baskin <buckbaskin@google.com>
2016-08-27 20:44:28 -0400

    Update setup to new version

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@ PyPI setup.py file
 
 from distutils.core import setup
 
+ver = '0.1.1'
+
 setup(name='couchpotato',
       packages=['couchpotato'],
-      version='0.1.0',
+      version=ver,
       description='Module for lazily evaluating Python functions',
       author='Buck Baskin',
       author_email='mobile.wbaskin+pypi@gmail.com',
       url='https://github.com/buckbaskin/couchpotato',
-      download_url='https://github.com/buckbaskin/couchpotato/tarball/0.1.0',
+      download_url='https://github.com/buckbaskin/couchpotato/tarball/%s' % ver,
       keywords=['couchpotato', 'lazy'],
       classifiers=[],)
